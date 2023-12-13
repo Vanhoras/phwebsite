d3da1430736deea35cca336bda31015b2167afb7
Vanhoras <phielsche@t-online.de>
2023-12-13 16:07:26 +0100

    added references title

--- src/views/WebsitesView.vue
+++ src/views/WebsitesView.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="websites_view">
+    <h2 class="websites__title">REFERENCES</h2>
     <WebsitePreview v-for="(website, index) in websites" :key="website.id" :website="website" :direction-left="(index % 2) == 0"/>
   </div>
 </template>
-  
+
 <script setup lang="ts">
   import { useWebsiteStore } from '@/stores/websiteStore';
   import type { Website } from '@/types/website';
@@ -22,6 +23,14 @@
       max-width: 110rem;
       margin: auto;
       padding-top: 7.5rem;
+  }
+
+  .websites__title {
+    margin: auto;
+    text-align: center;
+    font-size: 6rem;
+    font-weight: bold;
+    margin-bottom: 5rem;
   }
 
 </style>

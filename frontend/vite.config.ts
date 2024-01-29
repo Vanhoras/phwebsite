102ef03df74c5f6f78abc2ddfa4ee488d7c5d32e
Vanhoras <phielsche@t-online.de>
2024-01-29 21:07:30 +0100

    fixing build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: {
+        transformAssetUrls: {
+            includeAbsolute: false,
+        },
+      },
+    }),
   ],
   resolve: {
     alias: {
